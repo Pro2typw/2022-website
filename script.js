35a368bafb6eb689859d8dfe28ebfe459cee71dc
Ethan K <hambugerethan@gmail.com>
2023-01-01 18:55:21 -0600

    Update script.js

--- script.js
+++ script.js
@@ -7,12 +7,37 @@ window.addEventListener('scroll', () => {
         navbar.classList.remove('scrolled')
     }
 })
-window.addEventListener("scroll", function () {
+window.addEventListener("scroll", function (event) {
+   
     const distance = window.scrollY;
     $(".section-two.wrapper.left.container.bg").css('transform', `translateY(${distance *
         0.2}px)`);
     $(".section-two.wrapper.left.container").css('transform', `translateY(${distance * 0.3}px)`);
 });
+document.addEventListener("scroll", onScroll);
+function onScroll(event){
+  var element = document.querySelector('#in-title');
+  var line = document.querySelector('#in-line');
+  var btn = document.querySelector('#in-button');
+  var y = window.pageYOffset;
+  
+  if(y >= 700)
+  {
+    setTimeout(function(){
+    element.classList.add("left--state-in");
+      element.classList.remove("hidden");
+  }, 100)
+    setTimeout(function(){
+    line.classList.add("left--state-in");
+      line.classList.remove("hidden");
+  }, 200)
+    setTimeout(function(){
+    btn.classList.add("left--state-in");
+      btn.classList.remove("hidden");
+  }, 300)
+    
+  }
+}
 
 // q: how do I make a new git?
 // a: git init
