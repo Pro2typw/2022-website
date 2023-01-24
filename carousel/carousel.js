376e5ec6efe1f474f936e231495b16ecd3253ff7
DankBoi293 <hambugerethan@gmail.com>
2023-01-23 19:39:37 -0600

    Added a carousel!
    Website is 90% complete, just need assets.

---
+++ carousel/carousel.js
@@ -0,0 +1,54 @@
+
+
+// current slide counter
+// select next slide button
+// Select all slides
+const slides = document.querySelectorAll(".robot-slide");
+var wrapper = document.querySelector('.robots');
+//var slides = documents.querySelectorAll("robot-slide");
+var totalSlides = slides.length;
+// loop through slides and set each slides translateX
+slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${indx * 100}%)`;
+});
+
+// select next slide button
+const nextSlide = document.querySelector(".arrow-2");
+
+// current slide counter
+let curSlide = 0;
+// maximum number of slides
+let maxSlide = slides.length - 1;
+
+// add event listener and navigation functionality
+nextSlide.addEventListener("click", function () {
+    // check if current slide is the last and reset current slide
+    if (curSlide === maxSlide) {
+        curSlide = 0;
+    } else {
+        curSlide++;
+    }
+
+    //   move slide by -100%
+    slides.forEach((slide, indx) => {
+        slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+    });
+});
+
+// select next slide button
+const prevSlide = document.querySelector(".arrow-1");
+
+// add event listener and navigation functionality
+prevSlide.addEventListener("click", function () {
+    // check if current slide is the first and reset current slide to last
+    if (curSlide === 0) {
+        curSlide = maxSlide;
+    } else {
+        curSlide--;
+    }
+
+    //   move slide by 100%
+    slides.forEach((slide, indx) => {
+        slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+    });
+});
